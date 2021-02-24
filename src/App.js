966573b19f3440b1e18d4ab208e15f1174cad204
BerkantAkcil <43740214+BerkantAkcil@users.noreply.github.com>
2021-02-24 23:17:10 +0300

    İnitial upload

---
+++ src/App.js
@@ -0,0 +1,12 @@
+import './App.css';
+import PopularMovies from './Components/PopularMovies';
+
+function App() {
+  return (
+    <>
+     <PopularMovies />
+    </>
+  );
+}
+
+export default App;
